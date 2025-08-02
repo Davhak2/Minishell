4fa535d450245857a4f1746b542d548515e6106f
Davhak2 <davidhakobyan014@gmail.com>
2025-08-02 23:32:04 +0400

    REFINE: Implement expand_tilde function for tilde expansion in ft_echo; enhance argument handling for improved output formatting.

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -1,12 +1,78 @@
 #include "builtins.h"
-#include "stdio.h"
+#include "expander.h"
 #include "libft.h"
+
+char	*expand_tilde(char *arg, char **envp)
+{
+	char	*home;
+	char	*expanded;
+	char	*user;
+	char	*default_home;
+	char	*pwd_value;
+
+	if (!arg || *arg != '~')
+		return (ft_strdup(arg));
+	if (*(arg + 1) == '+' && (*(arg + 2) == '\0' || *(arg + 2) == '/'))
+	{
+		pwd_value = get_env_value("PWD", envp);
+		if (!pwd_value)
+			return (ft_strdup(arg));
+
+		if (*(arg + 2) == '\0')
+			expanded = ft_strdup(pwd_value);
+		else
+			expanded = ft_strjoin(pwd_value, arg + 2);
+		free(pwd_value);
+		return (expanded);
+	}
+	if (*(arg + 1) == '-' && (*(arg + 2) == '\0' || *(arg + 2) == '/'))
+	{
+		pwd_value = get_env_value("OLDPWD", envp);
+		if (!pwd_value)
+			return (ft_strdup(arg));
+
+		if (*(arg + 2) == '\0')
+			expanded = ft_strdup(pwd_value);
+		else
+			expanded = ft_strjoin(pwd_value, arg + 2);
+		free(pwd_value);
+		return (expanded);
+	}
+	if (*(arg + 1) == '\0' || *(arg + 1) == '/')
+	{
+		home = get_env_value("HOME", envp);
+		if (!home)
+		{
+			user = get_env_value("USER", envp);
+			if (user)
+			{
+				default_home = ft_strjoin("/home/", user);
+				free(user);
+				if (default_home)
+					home = default_home;
+			}
+		}
+		if (!home)
+			return (ft_strdup(arg));
+
+		if (*(arg + 1) == '\0')
+			expanded = ft_strdup(home);
+		else
+			expanded = ft_strjoin(home, arg + 1);
+		free(home);
+		return (expanded);
+	}
+	return (ft_strdup(arg));
+}
 
 int	ft_echo(char **args, t_shell *shell)
 {
-	int i = 1;
-	int newline = 1;
+	int		i;
+	int		newline;
+	char	*expanded;
 
+	i = 1;
+	newline = 1;
 	if (args[1] && ft_strcmp(args[1], "-n") == 0)
 	{
 		newline = 0;
@@ -15,12 +81,18 @@ int	ft_echo(char **args, t_shell *shell)
 
 	while (args[i])
 	{
-		printf("%s", args[i]);
+		if (args[i][0] == '~' && shell && shell->envp)
+		{
+			expanded = expand_tilde(args[i], *(shell->envp));
+			printf("%s", expanded);
+			free(expanded);
+		}
+		else
+			printf("%s", args[i]);
 		if (args[i + 1])
 			printf(" ");
 		i++;
 	}
-
 	if (newline)
 		printf("\n");
 	return (0);
