f06995b9277eca851c88bd2d4558935acca0ec0a
Garnik <g.nersisyann@gmail.com>
2025-08-07 15:14:42 +0400

    Removed some TODOs

--- includes/signals.h
+++ includes/signals.h
@@ -6,8 +6,8 @@
 
 extern volatile sig_atomic_t	g_received_signal;
 
-void							init_signals(void); // TODO: fix CTRL+C while running command, example: sleep 5 then CTRL+C
-void							setup_heredoc_signals(void); // TODO: fix heredoc CTRL+C
+void							init_signals(void);
+void							setup_heredoc_signals(void);
 void							restore_signals(void);
 void							sigint_handler(int sig);
 void							sigint_heredoc_handler(int sig);
